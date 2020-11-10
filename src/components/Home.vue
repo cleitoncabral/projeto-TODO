07a7a8afe261491d7f1c207cfadd88f38bbd49bf
Cleiton Cabral <cleitoncabral177@gmail.com>
2020-11-10 16:44:28 -0300

    Iniciando Projeto

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+<section>
+    <div>
+      <Header />
+    </div>
+  <section class="home">
+    <div>
+      <Menu />
+    </div>
+    <div>
+      <Content />
+    </div>
+  </section>
+
+</section>
+</template>
+
+<script>
+import Header from './Header/Header'
+import Menu from './Menu/Menu.vue'
+import Content from './Content/Content'
+export default {
+  components:{Header, Menu, Content}
+}
+</script>
+
+<style>
+.home{
+  display: grid;
+  grid-template-columns: 20% 80%;
+  width: 100%;
+  height: 100vh;
+}
+
+
+</style>
